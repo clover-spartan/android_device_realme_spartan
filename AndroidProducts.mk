680e201ede5009249f298a9cf272246cecee9991
provasish <iambumba3@gmail.com>
2026-02-19 09:27:36 +0100

    spartan: Bringup clover project
    Signed-off-by: provasish <iambumba3@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_spartan.mk
+    $(LOCAL_DIR)/clover_spartan.mk
--- clover_spartan.mk
+++ clover_spartan.mk
@@ -1,4 +1,4 @@
-#
+
 # Copyright (C) 2018 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
@@ -13,9 +13,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/realme/spartan/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/clover/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_spartan
+# Clover flags
+CLOVER_BUILDTYPE := OFFICIAL
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_ENABLE_BLUR := true
+CLOVER_MAINTAINER := provasish
+TARGET_INCLUDE_PIXEL_LAUNCHER := true
+PRODUCT_NO_CAMERA := true
+
+PRODUCT_NAME := clover_spartan
 PRODUCT_DEVICE := spartan
 PRODUCT_MANUFACTURER := realme
 PRODUCT_BRAND := realme
@@ -26,7 +37,3 @@ PRODUCT_GMS_CLIENTID_BASE := android-oppo
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="RMX3371-user 14 UKQ1.230924.001 S.1d262cb-66b86-66b87 release-keys" \
     BuildFingerprint=realme/RMX3371/RE54E4L1:14/UKQ1.230924.001/S.1d262cb-66b86-66b87:user/release-keys \
-    DeviceName=RE54E4L1 \
-    DeviceProduct=RMX3371 \
-    SystemDevice=RE54E4L1 \
-    SystemName=RMX3371
